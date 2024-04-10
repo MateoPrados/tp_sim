8d747b15834be6232b2f1f294f4d0f0cd99f1aa3
Mateo <matetep@hotmail.com>
2024-04-10 14:27:40 -0300

    firstcommit

--- .vs/tp_sim/mainForm.Designer.cs
+++ .vs/tp_sim/mainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace tp_sim
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1197, 656);
+            this.Name = "mainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
